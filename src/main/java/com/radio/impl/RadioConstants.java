85d2e7ff1e2b17e762ddc884357a9e4ecdb2cc13
Jekhuligan <Jekhuligan@gmail.com>
2016-08-03 02:12:43 +0400

    First commit

---
+++ src/main/java/com/radio/impl/RadioConstants.java
@@ -0,0 +1,13 @@
+package com.radio.impl;
+
+/**
+ * Created by Jek on 03.08.16.
+ */
+public interface RadioConstants
+{
+    public final static String VLC_LIB = "VLC_LIB";
+    public final static String MEDIA_DIR = "MEDIA_DIR";
+    public final static String ADDRESS = "ADDRESS";
+    public final static String PORT = "PORT";
+
+}
